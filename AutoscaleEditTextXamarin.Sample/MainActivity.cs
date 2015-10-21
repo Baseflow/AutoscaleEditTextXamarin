07e0944d3f3e475703b61f96a271edcf25cfe394
Martijn van Dijk <mhvdijk@gmail.com>
2015-10-21 13:40:02 +0200

    Add initial version

---
+++ AutoscaleEditTextXamarin.Sample/MainActivity.cs
@@ -0,0 +1,20 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+namespace AutoscaleEditTextXamarin.Sample
+{
+    [Activity (Label = "AutoscaleEditTextXamarin.Sample", MainLauncher = true, Icon = "@mipmap/icon")]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate (Bundle savedInstanceState)
+        {
+            base.OnCreate (savedInstanceState);
+
+            // Set our view from the "main" layout resource
+            SetContentView (Resource.Layout.Main);
+        }
+    }
+}
+
+
